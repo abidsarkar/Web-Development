28e7c7aea699c0db376885329270da7164e31701
abidsarkar <abidsarkar0@gmail.com>
2024-10-20 13:35:48 +0600

    add js to function the cross and login button

---
+++ Login form/login from 3/javasctipt.js
@@ -0,0 +1,7 @@
+let login = document.getElementById("login");
+function closeMenu(){
+    login.classList.add("mainMenuHidden")
+}
+function openMenu(){
+    login.classList.remove("mainMenuHidden")
+}
